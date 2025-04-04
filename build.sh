a1b4f684c6816749a18e18fa922bb3427ddd2527
Pierce Brooks <piercebrooks@rocketmail.com>
2025-04-03 21:03:47 -0400

    fix cmake compilation

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 rm -rf $PWD/build
 mkdir -p $PWD/build
 cd $PWD/build
-cmake -G Xcode -DBUILD_INSTALL_PREFIX=../install -CMAKE_BUILD_TYPE=Debug ..
+cmake -G Xcode -DBUILD_INSTALL_PREFIX=../install -DCMAKE_BUILD_TYPE=Debug ..
 cmake --build . --target install --config Debug
 cd ..
 
